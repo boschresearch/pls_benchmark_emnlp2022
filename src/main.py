67da70bbad2d21024666bd998e4b485e16841453
Pujari Subhash Chandra (CR/PJ-AI-R26) <SubhashChandra.Pujari@de.bosch.com>
2022-12-08 21:03:10 +0100

    minor corrections in main.py

--- src/main.py
+++ src/main.py
@@ -26,12 +26,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-# TODO: remove this path variable
 project_base = "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/code/bosch-patent/"
 sys.path.append(os.path.join(project_base, "src"))
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)).split("bosch-patent")[0],
-                             "bosch-patent/baseline/sklearn-hierarchical-classification"))
-
 
 from experiments_pl.data_utils import load_embeddings, get_dataset_ids
 from generic_classification.experiments_patent_landscape import load_and_train
@@ -39,23 +35,6 @@ from generic_classification.experiments_patent_landscape import load_and_train
 tf.random.set_seed(42)
 random.seed(42)
 np.random.seed(42)
-
-# emb_filename_type_dict = {
-#     "l-g-n2v": "label_emb/graph_emb/sg_norm_weight_graph_emb.txt",
-#     "l-t-bert": "label_emb/text_emb/sg-all-SciBERT-ft-THMM.pkl"
-# }
-
-# dataset_bert_emb_filenames_dict = {
-#     "atz": "cont_embs/sci-ft-THMM/atz_txt_emb.txt",
-#     "rito": "cont_embs/sci-ft-THMM/rito_txt_emb.txt",
-#     "fi": "cont_embs/sci-ft-THMM/fi_txt_emb.txt"
-# }
-
-# dataset_partial_dict_map = {
-#     "atz": (50, None, 25),
-#     "fi": (50, 2001, 50),
-#     "rito": (50, None, 25)
-# }
 
 
 def get_logger(filename):
@@ -164,18 +143,11 @@ def run_config(config, split_index=None, mode="debug", cpc_code_filter_list=list
     metrics_heldout = list()
     metrics = list()
 
-
     content_fields = ["Title", "Abstract", "Claims", "Description"]
 
     doc_rep_prop = config["doc_rep_params"]["doc_rep_prop"]
 
     train_ids, dev_ids, test_ids, final_hold_out_set_ids = get_dataset_ids(split_index, config, k, logger=logger)
-
-    if train_sample_count:
-        #load train ids from the file and replace the train_ids.
-        random_files_dir = "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/dataset/patent_ls/random-ids-pl"
-        filename = os.path.join(random_files_dir, "rand_train_ids_%s_%s_%s.txt" % (config["dataset"], k, train_sample_count))
-        train_ids = [int(item.strip()) for item in open(filename).readlines()]
 
     load_and_train(config, data, train_ids, test_ids, dev_ids, final_hold_out_set_ids, 
                 doc_rep_prop, emb_dict, exp_dir, metrics, metrics_heldout, 
@@ -205,14 +177,12 @@ def main():
     if args.option == "1_split" and args.split >= 0:
         run_config(json.loads(open(args.cfg).read()),
                    split_index=args.split, 
-                #    option=args.option, 
                    mode=args.mode,
                    train_sample_count=args.train_sample_count)
 
     elif args.option == "1_cfg":
         run_config(json.loads(open(args.cfg).read()),
                    split_index=None, 
-                #    option=args.option, 
                    mode=args.mode,
                    train_sample_count=args.train_sample_count)
 
@@ -221,91 +191,17 @@ def main():
             config = json.loads(config_json.strip())
             run_config(config, 
                     split_index=None,
-                    # option=args.option, 
                     mode=args.mode,
                     train_sample_count=args.train_sample_count)
 
     elif args.option == "partial_split_cfg":
         run_config(json.loads(open(args.cfg).read()),
                    split_index=None, 
-                #    option=args.option, 
                     mode=args.mode,
                     train_sample_count=args.train_sample_count)
     else:
         ValueError("Unkown option : %s " % args.option)
 
-# TODO: remove the configuration from here at the end
-config = {
-    "input_dir_path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/dataset/patent_ls",
-    "output_dir_path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/output_dir",
-    "dataset_filename": "rito_2021-08-04_filtered.csv",
-    "split_dir": "splits-5/splits-rito",
-    "split-k": 5,
-    "dataset": "rito",
-    "doc_rep_params": {
-        "embs": [
-            {
-                "type": "bert",
-                "fields": [
-                    "Title",
-                    "Abstract"
-                ],
-                "path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/scibert-test",
-                "max_len": 512,
-                "label_text": "False",
-                "trainable": "True"
-            },
-            {
-                "type": "bert",
-                "fields": [
-                    "Claims"
-                ],
-                "path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/scibert-test",
-                "max_len": 512,
-                "label_text": "False",
-                "trainable": "True"
-            },
-            {
-                "type": "bert",
-                "fields": [
-                    "Description"
-                ],
-                "path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/scibert-test",
-                "max_len": 512,
-                "label_text": "False",
-                "trainable": "True"
-            },
-            # {
-            #     "type": "l-t-tfidf",
-            #     "path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/output_dir/label_emb/text_emb/tf_idf_label.pkl"
-            # }
-        ],
-        "doc_rep_prop": {
-            "cf_agg": "sum",
-            "mf_agg": "sum",
-            "doc_agg": "concat",
-            "cf_agg_layer_norm": "False",
-            "mf_agg_layer_norm": "False"
-        }
-    },
-    "model": "TMM",
-    "model_params": {
-        "model": "TMM",
-        "dense_layer_size": 50,
-        "dropout_rate": 0.25,
-        "learning_rate": 3e-05,
-        "epochs": 50,
-        "batch_size": 4,
-        "emb_agg": "concat",
-        "encoder_size": 768,
-        "kernel": "rbf"
-    },
-    "label_desc_file_path": "/fs/scratch/rng_cr_bcai_dl_students/r26/patent_classification/code/bosch-patent/output/hier_cpc/label.csv",
-    "exp_dir_prefix": "tacd",
-    "exp_dir": "/fs/scratch/rng_cr_bcai_dl/ujp5kor/output_dir/experiment-pl-code-release-emnlp-2022/experiments-bert"
-}
-
 
 if __name__ == "__main__":
-    # run_config(config, split_index=0, mode="debug", train_sample_count=50)
     main()
